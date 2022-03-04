9b717c351bc4b7b4b113fd5c4ef59b14368f3b4b
zakiafada32 <afadazaki32@gmail.com>
2022-03-05 01:59:42 +0700

    get token process - before   - GetStaticProps -> send req to internal api -> third party
    - after
      - GetStaticProps -> third party
    
    GetStaticProps always runs on the server and never on the client. It is safe to write the server-side code directly.
    Source https://nextjs.org/docs/basic-features/data-fetching/get-static-props#write-server-side-code-directly

--- libs/auth.ts
+++ libs/auth.ts
@@ -1,31 +1,40 @@
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const OMNI_URL = process.env.NEXT_PUBLIC_OMNI_URL;
+const OMNI_USERNAME = process.env.OMNI_USERNAME;
+const OMNI_PASSWORD = process.env.OMNI_PASSWORD;
 
-export interface AuthResult {
+interface AuthResult {
   code: number;
-  token: string;
+  data: {
+    token: string;
+    expired: string;
+  };
 }
 
 export type Token = string;
 
 export const auth = async (): Promise<Token> => {
   try {
-    const response = await fetch(BASE_URL + '/api/auth', {
-      method: 'GET',
+    const response = await fetch(OMNI_URL + '/api/v1/token/', {
+      method: 'POST',
       headers: {
-        'Content-type': 'application/json',
+        'content-type': 'application/json',
       },
+      body: JSON.stringify({
+        username: OMNI_USERNAME,
+        password: OMNI_PASSWORD,
+      }),
     });
 
     if (response.status !== 200) {
       throw new Error('auth failed');
     }
 
-    const data = (await response.json()) as AuthResult;
-    if (data.code !== 200) {
+    const result = (await response.json()) as AuthResult;
+    if (result.code !== 200) {
       throw new Error('auth failed');
     }
 
-    return data.token;
+    return result.data.token;
   } catch (error) {
     return '';
   }
